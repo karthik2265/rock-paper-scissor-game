7ab756da58935b7b360fa459b56b8c3f7e0f4457
iediong <ediongi6@hotmail.com>
2022-02-27 02:30:21 +0100

    Basic logic for modal at 375px

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   greeting: string = 'Hello Karthik 👋';
-  showRules: boolean = true;
+  showRules: boolean = false;
+
+  displayRules() {
+    this.showRules = true;
+    console.log('display rules modal');
+  }
+
+  closeRules(): void {
+    this.showRules = false;
+    console.log('close rules modal');
+  }
 }
